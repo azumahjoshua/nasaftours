7507669f3d9d399f5763027925f858eb650e19ae
azumahjoshua <azumahjoshua1086@gmail.com>
2025-08-15 09:05:23 +0000

    test: add dummy TestCase to fix unused import lint error

--- tourghanaapp/tests.py
+++ tourghanaapp/tests.py
@@ -1,3 +1,5 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+class DummyTest(TestCase):
+    def test_placeholder(self):
+        self.assertTrue(True)
